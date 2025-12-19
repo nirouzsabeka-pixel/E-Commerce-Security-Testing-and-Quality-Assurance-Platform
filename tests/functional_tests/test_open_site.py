6e73e757dbe55910f43ca9c463071ec09272ef17
nirouzsabeka-pixel <nirouzsabeka@gmail.com>
2025-12-19 23:13:20 +0200

    Implement open website automated test

--- tests/functional_tests/test_open_site.py
+++ tests/functional_tests/test_open_site.py
@@ -1,1 +1,14 @@
-# Open website test (Selenium)
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument("--start-maximized")
+
+service = Service(r"C:\chromedriver\chromedriver.exe")
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("http://demowebshop.tricentis.com")
+time.sleep(5)
+driver.quit()
